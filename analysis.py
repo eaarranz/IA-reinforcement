2e2c3facf3975f3d3d8857f6da5c7d0e864d4d81
Eudald Arranz Tresserra <eudald.arranz@nested.net>
2017-12-10 12:59:55 +0100

    Answers

--- analysis.py
+++ analysis.py
@@ -20,9 +20,22 @@
 # value iteration.
 
 def question3():
-    answerEpsilon = None
-    answerLearningRate = None
-    return answerEpsilon, answerLearningRate
+    return 'NOT POSSIBLE'
+    '''
+    The value of epsillon changes the randomness of the exploration. A low epsillon means that there is a low
+    chance of the agent doing a random action. A bigger one means a bigger chance of doing a random action.
+    So:
+    If the agent runs with a low epsillon, it will be prone to perform the safe action, that will be go back to the 
+    initial cell and take the reward.
+    If it runs with a bigger epsillon, close to 1, it will be prone to perform a random action at each state, 
+    exploring more the gird. 
+    The problem with the proposed statement is that with a completely random action there is a chance of 1/1024 
+    (1/4 actions ** 5 cells) to reach the final cell. With 50 training runs, it's very hard to get there, and once
+    it reach it, the q-value of going back can be higher than the one that we get reaching the final cell.
+    We can play with the learning factor:
+    An alpha close to 0 will make the agent not learn almost anything, one close to 1 will take only the recent values
+    into consideration.
+    '''
     # If not possible, return 'NOT POSSIBLE'
 
 if __name__ == '__main__':
